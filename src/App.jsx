5d2cad1f751e8ddd053ba63217676bca91028d05
Carolz87 <carolfranco.oliveira@gmail.com>
2025-01-21 11:06:48 -0300

    Estrutura base de Site
    Foram criados os componetes base de página
    
    Foram criados as páginas básicas
    
    Estruturadas as rotas básicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-     <H1>Hello world</H1>
+     <BrowserRouter>
+     <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
     </>
   )
 }
