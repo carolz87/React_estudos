a3415a7ced6c612b0257123a4dd6a4aeb84d1326
Carolz87 <carolfranco.oliveira@gmail.com>
2025-01-21 11:55:37 -0300

    Finalização de rota
    Foi editada a barra de navegação para funcionamento de links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ function Navbar() {
     return (<>
         <nav>
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
+                <li><a href="/">Home</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="contact">Contact</a></li>
             </ul>
         </nav>
         </>)
